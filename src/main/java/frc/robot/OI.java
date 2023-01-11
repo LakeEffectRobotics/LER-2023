03b7a03a429d1334da82111ca17170949fd37fb6
Gregory Kelly <git@gregk.ca>
2023-01-10 20:02:46 -0500

    Add empty classes for OI and Robotmap

---
+++ src/main/java/frc/robot/OI.java
@@ -0,0 +1,23 @@
+package frc.robot;
+
+/**
+ * This class holds configurations for the Operator Interface (so joysticks/controllers)
+ */
+public class OI {
+    
+    /** Driver Station ports controllers are attached to */
+    private static class PORTS {
+
+    }
+
+    /** Buttons on the driver sticks/controller */
+    private static class DRIVER_MAP {
+
+    }
+
+    /** Buttons on the operator controller */
+    private static class OPERATOR_MAP {
+        
+    }
+
+}
---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,20 @@
+package frc.robot;
+
+/**
+ * Mapping and creation of hardware on the robot
+ */
+public class RobotMap {
+    
+    /**
+     * Inner class to hold CAN ID constants.
+     */
+    private class CAN {
+
+    }
+
+    // Static initializer will be run on first reference to RobotMap
+    static {
+        // It's a bit ambiguous what config belongs here vs elsewhere, but a good rule of thumb is that anything that 
+        // isn't implicitly part of a motor's name (so PID constants, ramp settings, etc) belong in subsystems
+    }
+}
