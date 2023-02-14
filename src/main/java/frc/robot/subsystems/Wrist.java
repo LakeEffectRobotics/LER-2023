c68885ee6e922d1e6391d961e993a2d6c48f0327
celery6 <71736183+celery6@users.noreply.github.com>
2023-02-13 19:33:25 -0500

    WIP: add pot, limits, pid #5

--- src/main/java/frc/robot/subsystems/Wrist.java
+++ src/main/java/frc/robot/subsystems/Wrist.java
@@ -1,21 +1,55 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkMaxAnalogSensor;
+import com.revrobotics.SparkMaxLimitSwitch;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax.ControlType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Wrist extends SubsystemBase {
     CANSparkMax wristController;
 
+    public SparkMaxLimitSwitch forwardLimit;
+    public SparkMaxLimitSwitch reverseLimit;
+
+    private SparkMaxAnalogSensor pot;
+
+    private SparkMaxPIDController pidController;
+
+    private static final double kF = 0;
+    private static final double kP = 0;
+    private static final double kI = 0;
+    private static final double kD = 0;
+    private static final double MAX_OUTPUT = -1;
+    private static final double MIN_OUTPUT = -1;
+
     public Wrist(CANSparkMax controller) {
-        this.wristController = controller;
+        wristController = controller;
+        forwardLimit = wristController.getForwardLimitSwitch(SparkMaxLimitSwitch.Type.kNormallyOpen);
+        reverseLimit = wristController.getReverseLimitSwitch(SparkMaxLimitSwitch.Type.kNormallyOpen);
+
+        pot = wristController.getAnalog(SparkMaxAnalogSensor.Mode.kAbsolute);
+
+        pidController = wristController.getPIDController();
+
+        // Use potentiometer for PID control
+        pidController.setFeedbackDevice(pot);
+
+        // set PID coefficients
+        pidController.setP(kP);
+        pidController.setI(kI);
+        pidController.setD(kD);
+        pidController.setFF(kF);
+        pidController.setOutputRange(MIN_OUTPUT, MAX_OUTPUT);
     }
 
     /**
      * 
-     * @param angle
+     * @param position -1 to 1
      */
-    public void goToAngle(double angle) {
-
+    public void goToPosition(double position) {
+        pidController.setReference(position, ControlType.kPosition);
     }
 }
