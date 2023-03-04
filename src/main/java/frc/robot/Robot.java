122640463a0f88c211b8b1cd7111f2337f798380
Liam Pilson <liamphone0@gmail.com>
2023-03-04 13:51:11 -0500

    added lights subsystem

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,11 +7,14 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.Lights;
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
+
+  public static final Lights lights = new Lights();
 
   @Override
   public void robotInit() {
