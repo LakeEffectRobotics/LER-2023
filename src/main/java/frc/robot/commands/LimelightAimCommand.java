315a6d763cd54603f3fa8afa38acc09fc5d6106b
celery6 <71736183+celery6@users.noreply.github.com>
2023-03-10 20:03:24 -0500

    make limelight aim command read from target selection cube/cone

--- src/main/java/frc/robot/commands/LimelightAimCommand.java
+++ src/main/java/frc/robot/commands/LimelightAimCommand.java
@@ -4,20 +4,29 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Limelight;
+import frc.robot.subsystems.TargetSelection;
 
 public class LimelightAimCommand extends CommandBase {
     Limelight limelight;
     Drivetrain drivetrain;
+    TargetSelection targetSelection;
+
     private PIDController pidController = new PIDController(0.01, 0.0, 0.0);
 
-    public LimelightAimCommand(Limelight limelight, Drivetrain drivetrain) {
+    public LimelightAimCommand(Limelight limelight, Drivetrain drivetrain, TargetSelection targetSelection) {
         this.limelight = limelight;
         this.drivetrain = drivetrain;
+        this.targetSelection = targetSelection;
     }
 
     @Override
     public void initialize() {
         pidController.setSetpoint(0);
+        if (targetSelection.getSelectedNode().getType().name() == "CUBE") {
+            limelight.setPipeline(Limelight.Pipeline.CUBE);
+        } else {
+            limelight.setPipeline(Limelight.Pipeline.CONE);
+        }
     }
 
     @Override
