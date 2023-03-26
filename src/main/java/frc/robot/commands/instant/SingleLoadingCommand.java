ab14f44b0e173fcacd28ab40247a8eac6080b4db
LER-programming <github.lakeffectrobotics@gmail.com>
2023-03-26 08:36:12 -0400

    no printing

--- src/main/java/frc/robot/commands/instant/SingleLoadingCommand.java
+++ src/main/java/frc/robot/commands/instant/SingleLoadingCommand.java
@@ -1,8 +1,6 @@
 package frc.robot.commands.instant;
 
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.TargetSelection;
 import frc.robot.subsystems.Wrist;
@@ -17,14 +15,9 @@ public class SingleLoadingCommand extends SequentialCommandGroup {
         
         // drop tellescope first, then set wrist because telescope is going to 0 and its better to move wrist there than if its still up
         addCommands(
-            Commands.runOnce(() -> System.out.println(wristAngle)),
-
             new RaiseArmCommand(arm, true),
-            Commands.runOnce(() -> System.out.println("wekfl")),
 
             new SetTelescopeCommand(arm, telescopePosition).withTimeout(0.3),
-
-            Commands.runOnce(() -> System.out.println("ahhhh")),
 
             new SetWristAngleCommand(wrist, wristAngle)
         );
