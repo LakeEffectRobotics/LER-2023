c47d58f6fd3de477413f01e1a2e0b6f59f9f68db
celery6 <71736183+celery6@users.noreply.github.com>
2023-04-05 11:04:55 -0400

    add 1 cone 1 cube auto chooser

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -105,7 +105,7 @@ public class RobotContainer {
     autoChooser.addOption("score cone", new AutoScoreConeCommand(arm, wrist, claw));
     
     // CONES!!
-    autoChooser.addOption("flat 1 cone 1 cube", createPathUtils.createPathCommand("flat 1 cone 1 cube", 1.7, 1));
+    autoChooser.addOption("flat 1 cone 1 cube", createPathUtils.createPathCommand("flat 1 cone 1 cube", 1.4, 1, true));
 
     Shuffleboard.getTab("my favourite tab")
       .add(autoChooser)
